0d625870b25ca932321b6a7ad5e9c7f02ad81d1e
Steve Forkin <steve@netflakes.co>
2018-12-04 18:10:01 +1100

    updated

--- lib/client.ex
+++ lib/client.ex
@@ -1,8 +1,8 @@
 defmodule DropboxEx.Client do
 
-  defstruct client_id: nil, access_token: nil, base_url: nil, upload_url: nil
+  defstruct client_id: nil, access_token: nil
 
-  def new(access_token, base_url, upload_url) do
-    %DropboxEx.Client{ access_token: access_token, base_url: base_url, upload_url: upload_url }
+  def new(access_token) do
+    %DropboxEx.Client{access_token: access_token}
   end
 end
